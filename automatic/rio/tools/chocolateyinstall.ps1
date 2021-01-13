fb7cbf2b9f3a361f1b9c96b4be49bdada8e8784b
Erwin Kersten <erwin.kersten@gmail.com>
2021-01-13 23:24:47 +0000

    AU: 1 updated - rio [skip ci] https://gist.github.com/6154569d713d1d87b0a36e5781d7ef3c

--- automatic/rio/tools/chocolateyinstall.ps1
+++ automatic/rio/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = '-'
-$checksum64     = '-'
-$checksumType64 = '-' 
+$url64          = 'https://github.com/rancher/rio/releases/download/v0.8.0/rio-windows-amd64'
+$checksum64     = '83dfb50c51ed5b5496b65332f9677e546cb7da953c21f3cb93feae06def8b7e7'
+$checksumType64 = 'sha256' 
 
 $packageArgs = @{
   softwareName   = 'rio'
