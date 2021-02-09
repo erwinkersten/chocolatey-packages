324b3287b22255114382ff856e0f0f4fe2e43d6b
erwinkersten <erwin.kersten@gmail.com>
2021-02-09 21:59:45 +0100

    Update github version detect au_GetLatest

--- automatic/k0s/update.ps1
+++ automatic/k0s/update.ps1
@@ -3,7 +3,7 @@ import-module au
 # Using latest in te release url to prevent test releases being detected. 
 $owner = 'k0sproject'
 $repo = 'k0s' 
-$releases = 'https://github.com/$owner/$repo/releases'
+$releases = "https://github.com/"+$owner+"/"+$repo+"/releases"
 
 function global:au_SearchReplace {
     @{
@@ -21,11 +21,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'k0s' -NoSuffix}
-function g-
-lobal:au_GetLatest {
+function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regex = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}$"
-    $url = $download_page.links | Where-Object href -match $artifact | Select-Object -First 1 -expand href
+    $regex   = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}$"
+    $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
     $version = $url -split '\/|v' | Select-Object -Last 1
     $url = "https://github.com/$owner/$repo/releases/download/v$version/k0s-v$version-amd64.exe"
     
