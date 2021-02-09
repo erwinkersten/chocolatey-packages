f3a7b4c9f85500dab3f2e2d8d64bda33fabcd96a
erwinkersten <erwin.kersten@gmail.com>
2021-02-09 21:40:28 +0100

    Initial k0s package

---
+++ automatic/k0s/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64          = 'x'
+$checksum64     = 'X'
+$checksumType64 = 'sha256' 
+
+$packageArgs = @{
+  softwareName   = 'k0s'
+  packageName    = 'k0s'
+  FileFullPath   = $toolsDir+'\k0s.exe'  
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64 
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyWebFile @packageArgs
---
+++ automatic/k0s/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+# $global:au_Force = $true
+# Using latest in te release url to prevent test releases being detected. 
+$owner = 'k0sproject'
+$repo = 'k0s' 
+$releases = 'https://github.com/$owner/$repo/releases'
+
+function global:au_SearchReplace {
+    @{
+        '.\legal\VERIFICATION.txt' = @{
+            "(?i)(url:).*"       = "`$1'$($Latest.URL64)'"
+            "(?i)(checksum:).*"  = "`$1'$($Latest.Checksum64)'"
+            "(?i)(checksumType:).*"  = "`$1'$($Latest.ChecksumType64)'"
+        } 
+        '.\tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'k0s' -NoSuffix}
+function g-
+lobal:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}$"
+    $url = $download_page.links | Where-Object href -match $artifact | Select-Object -First 1 -expand href
+    $version = $url -split '\/|v' | Select-Object -Last 1
+    $url = "https://github.com/$owner/$repo/releases/download/v$version/k0s-v$version-amd64.exe"
+    
+    return @{ 
+        URL64 = $url
+        Version = $version 
+    }
+
+}
+
+Update-Package -ChecksumFor none
