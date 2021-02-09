229c758f9ca51bee9fca4019922a78580b551bcb
Erwin Kersten <erwin.kersten@gmail.com>
2021-02-09 21:54:08 +0000

    AU: 1 updated - k0sctl [skip ci] https://gist.github.com/6154569d713d1d87b0a36e5781d7ef3c

--- automatic/k0sctl/tools/chocolateyinstall.ps1
+++ automatic/k0sctl/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'x'
-$checksum64     = 'x'
+$url64          = 'https://github.com/k0sproject/k0sctl/releases/download/v0.4.0/k0sctl-win-x64.exe'
+$checksum64     = '03FA8C7C0E2F810B107FB39E4D3E8CD570699B83FE6F57306A90407D64CB8049'
 $checksumType64 = 'sha256' 
 
 $packageArgs = @{
