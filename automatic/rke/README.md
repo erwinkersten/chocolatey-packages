3e0d81cf6b394e6681f1024841c56b41f1e998b9
erwinkersten <erwin.kersten@gmail.com>
2021-03-17 12:38:18 +0100

    Add RKE package

---
+++ automatic/rke/README.md
@@ -0,0 +1,10 @@
+# <img src="https://rawcdn.githack.com/erwinkersten/chocolatey-packages/f91662c8b0c331926ad30ec35156f8bb20cf7daf/icons/rke.svg" height="100"/>[](https://rancher.com/products/rke)
+
+## Rancher Kubernetes Engine (RKE)
+
+Rancher Kubernetes Engine (RKE) is a CNCF-certified Kubernetes distribution that runs entirely within Docker containers. It works on bare-metal and virtualized servers. With RKE, the installation and operation of Kubernetes is both simplified and easily automated, and it’s entirely independent of the operating system and platform you’re running.
+
+Website: https://rancher.com/products/rke/
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two please contact the maintainer(s) and let them know the package is no longer updating correctly.
+    
---
+++ automatic/rke/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+# $global:au_Force = $true
+# Using latest in te release url to prevent test releases being detected. 
+$releases = 'https://github.com/rancher/rke/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        '.\legal\VERIFICATION.txt' = @{
+            "(?i)(url:).*"       = "`$1'$($Latest.URL64)'"
+            "(?i)(checksum:).*"  = "`$1'$($Latest.Checksum64)'"
+            "(?i)(checksumType:).*"  = "`$1'$($Latest.ChecksumType64)'"
+        } 
+    }
+}
+
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'k3d' -NoSuffix}
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $artifact  = "rke_windows-amd64.exe"
+    $url = $download_page.links | Where-Object href -match $artifact | Select-Object -First 1 -expand href
+    $version = ([regex]::Match($url,'/rancher/rke/releases/download/v(.+)/'+$artifact)).Captures.Groups[1].value
+    $url = 'https://github.com' + $url
+
+    return @{ 
+        URL64 = $url
+        Version = $version 
+    }
+
+}
+Update-Package -ChecksumFor none
