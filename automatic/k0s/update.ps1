f5b40a35dbe7215ebddf8419abe5de3f7b658464
Erwin Kersten <erwin.kersten@gmail.com>
2021-05-05 13:23:52 +0200

    Fix version in dl url

--- automatic/k0s/update.ps1
+++ automatic/k0s/update.ps1
@@ -19,10 +19,13 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'k0s' -NoSuffix}
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regex   = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}\+k0s\.{0,1}\d{0,4}$"
+    $regex   = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}"
     $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
     $version = $url -split '\/|v' | Select-Object -Last 1
-    $url = "https://github.com/$owner/$repo/releases/download/v$version/k0s-v$version-amd64.exe"
+    $fileversion = $version -split '\/|%2B' | Select-Object -Last 2
+    $k8sver= $fileversion[0]
+    $k0sver = $fileversion[1]
+    $url = "https://github.com/$owner/$repo/releases/download/v$version/k0s-v$k8sver+$k0sver-amd64.exe"
     
     return @{ 
         URL64 = $url
