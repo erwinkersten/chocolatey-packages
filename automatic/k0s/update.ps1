af77f1909b5fe424e50e40dc0c1232dd21c2aafb
Erwin Kersten <erwin.kersten@gmail.com>
2021-05-05 10:48:55 +0200

    Fix version regex matching

--- automatic/k0s/update.ps1
+++ automatic/k0s/update.ps1
@@ -4,6 +4,7 @@ import-module au
 $owner = 'k0sproject'
 $repo = 'k0s' 
 $releases = "https://github.com/"+$owner+"/"+$repo+"/releases/latest"
+https://github.com/k0sproject/k0s/releases/download/v1.21.0%2Bk0s.0/k0s-v1.21.0+k0s.0-amd64.exe
 
 function global:au_SearchReplace {
     @{
@@ -18,7 +19,7 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'k0s' -NoSuffix}
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regex   = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}$"
+    $regex   = "\/$owner\/$repo\/releases\/tag\/v\d{1,4}\.{0,1}\d{0,4}\.{0,1}\d{0,4}\+k0s\.{0,1}\d{0,4}$"
     $url     = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
     $version = $url -split '\/|v' | Select-Object -Last 1
     $url = "https://github.com/$owner/$repo/releases/download/v$version/k0s-v$version-amd64.exe"
